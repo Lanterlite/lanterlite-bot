2fd46ccddc0f0f1838ee1f21ab542f6ce671df68
ifanramadhani <ifan.ra@outlook.co.id>
2017-05-03 11:44:04 +0700

    test

--- src/main/java/com/dicoding/menirukanmu/LineBotController.java
+++ src/main/java/com/dicoding/menirukanmu/LineBotController.java
@@ -132,40 +132,45 @@ public class LineBotController
 			
 			if(parts[0].equals("bot")){
 				if(parts[1].equals("informasi")){
+					// try {
+                        // getMessageData(dataAdzan, idTarget);
+                    // } catch (IOException e) {
+                        // System.out.println("Exception is raised ");
+                        // e.printStackTrace();
+                    // }
+					final String filePath = "BotProduktif.json";
+						
 					try {
-						final String filePath = "BotProduktif.json";
-							
+						// read the json file
+						FileReader reader = new FileReader("BotProduktif.json");
+
+						JSONParser jsonParser = new JSONParser();
+						JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+
+						// get a String from the JSON object
+						String subuh = (String) jsonObject.get("subuh");
+						String dzuhur = (String) jsonObject.get("dzuhur");
+						String ashr = (String) jsonObject.get("ashr");
+						String maghrib = (String) jsonObject.get("maghrib");
+						String isya = (String) jsonObject.get("isya");
+
+						String dataAdzan = new StringBuilder().append("Subuh: ").append(subuh).append("\nDzuhur: ").append(dzuhur).append("\nAshr: ").append(ashr).append("\nMaghrib: ").append(maghrib).append("\nIsya: ").append(isya).toString();
 						try {
-							// read the json file
-							FileReader reader = new FileReader("BotProduktif.json");
-
-							JSONParser jsonParser = new JSONParser();
-							JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
-
-							// get a String from the JSON object
-							String subuh = (String) jsonObject.get("subuh");
-							String dzuhur = (String) jsonObject.get("dzuhur");
-							String ashr = (String) jsonObject.get("ashr");
-							String maghrib = (String) jsonObject.get("maghrib");
-							String isya = (String) jsonObject.get("isya");
-
-							String dataAdzan = new StringBuilder().append("Subuh: ").append(subuh).append("\nDzuhur: ").append(dzuhur).append("\nAshr: ").append(ashr).append("\nMaghrib: ").append(maghrib).append("\nIsya: ").append(isya).toString();
 							getMessageData(dataAdzan, idTarget);
-
-						} catch (FileNotFoundException ex) {
-							ex.printStackTrace();
-						} catch (IOException ex) {
-							ex.printStackTrace();
-						} catch (ParseException ex) {
-							ex.printStackTrace();
-						} catch (NullPointerException ex) {
-							ex.printStackTrace();
+						} catch (IOException e) {
+							System.out.println("Exception is raised ");
+							e.printStackTrace();
 						}
-						// getMessageData(dataAdzan, idTarget);
-                    } catch (IOException e) {
-                        System.out.println("Exception is raised ");
-                        e.printStackTrace();
-                    }
+
+					} catch (FileNotFoundException ex) {
+						ex.printStackTrace();
+					} catch (IOException ex) {
+						ex.printStackTrace();
+					} catch (ParseException ex) {
+						ex.printStackTrace();
+					} catch (NullPointerException ex) {
+						ex.printStackTrace();
+					}
 				}
 				else if(parts[1].equals("reminder")){
 					try {
